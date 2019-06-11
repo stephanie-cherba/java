fa07025e97f8a788e6155f3251fe223433829526
Stephanie <stephlane544@gmail.com>
2019-06-11 10:30:33 -0600

    completed next task

---
+++ tasks/level-3/lecture-8/moreConversations.java
@@ -0,0 +1,8 @@
+public class moreConversations{
+    public static void main(String[] args){
+        System.out.println(getFeetFromInches(28));
+    }
+    public static int getFeetFromInches(int inches){
+        return (inches / 12);
+    }
+}
